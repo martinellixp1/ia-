ddd521cc7686cefed212dfc2472ff93000d92c52
martinellixp1 <miguel.martinelli@escola.pr.gov.br>
2026-08-18 12:16:56 -0300

    .

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const caixaprincipal = document.querySelector(".caixa-principal")
 const caixaperguntas = document.querySelector(".caixa-pergutas")
 const caixaalternativas = document.querySelector(".caixa-alternativas")
-const caixaresultados= document.querySelector(".caixa-resultados")
-const textoresultados = document.querySelector(".texto-resultados")
+const caixaresultado= document.querySelector(".caixa-resultados")
+const textoresultado = document.querySelector(".texto-resultados")
 
 
 
