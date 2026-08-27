bf67182d16730c19880eb785c7efc65d7d991481
otavionascimentosilva-sketch <otavio.nascimento.silva@escola.pr.gov.br>
2026-08-27 09:23:10 -0300

    jui

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ function mostrarpergunta(){
 }
 
 function mostraalternativas(){
-    for (const alternativa os perguntaatual.alternativas){
+    for (const alternativa of perguntaAtual.alternativas){
         const  botaoalternativas = document.createElement("button")
         botaoalternativas.textContent = alternativa.texto
         botaoalternativas.addEventListener("click", ()=> respostaSelecionada(alternativa))
