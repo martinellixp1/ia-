6f83a81bcd377a229bc11cac97b1afc43e4696bc
otavionascimentosilva-sketch <otavio.nascimento.silva@escola.pr.gov.br>
2026-09-01 12:09:31 -0300

    ,

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const caixaPrincipal = document.querySelector(".caixa-principal")
-const caixaPerguntas = document.querySelector(".caixa-pergutas")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
 const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultado= document.querySelector(".caixa-resultados")
 const textoResultado = document.querySelector(".texto-resultados")
