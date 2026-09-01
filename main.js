1bb883abcb6dd81174f5c9bf14da2c4e7e36be57
otavionascimentosilva-sketch <otavio.nascimento.silva@escola.pr.gov.br>
2026-09-01 12:11:19 -0300

    j

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 function mostraResultado(){
     caixaPerguntas.textContent ="Em 2049..."
-    textoResultado.textContent = historiafinal
+    textoResultado.textContent = historiaFinal
     caixaAlternativas.textContent ="";
 }
 
