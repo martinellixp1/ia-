748d5a0ded00edb0cc6b418bae9b8ccb158a44f9
martinellixp1 <miguel.martinelli@escola.pr.gov.br>
2026-09-17 09:27:44 -0300

    .

--- js/pergunta.js
+++ js/pergunta.js
@@ -5,14 +5,14 @@ export const perguntas = [
             {
                 texto: "Fica imóvel, observando tudo com cautela.",
                 afirmacao: [
-                    "Você percebe que a calma ajuda a entender melhor o ambiente ao seu redor."
+                    "Você percebe que a calma ajuda a entender melhor o ambiente ao seu redor.",
                     "você é arisco e sai correndo pela casa"
                 ]
             },
             {
                 texto: "Começa a explorar cada canto farejando tudo.",
                 afirmacao: [
-                    "Sua curiosidade felina te levou a descobrir muitos segredos da casa."
+                    "Sua curiosidade felina te levou a descobrir muitos segredos da casa.",
                     "sair correndo até achar uma estranha"
                 ]
             }
@@ -24,14 +24,14 @@ export const perguntas = [
             {
                 texto: "Empurra a porta com a pata e entra devagar.",
                 afirmacao: [
-                    "Sua coragem te recompensou com um delicioso pedaço de peixe."
+                    "Sua coragem te recompensou com um delicioso pedaço de peixe.",
                     "você começa a farmar aura fazendo six seven na frete da porta e com isso você mogga a porta e ela se abre"
                 ]
             },
             {
                 texto: "Fica miando perto da porta até que alguém apareça.",
                 afirmacao: [
-                    "Descobriu que miar pode atrair atenção e carinho dos humanos."
+                    "Descobriu que miar pode atrair atenção e carinho dos humanos.",
                     "você entra na porta e vê um idoso na cadeira de rodas ele olha pra vc e fica bravo e solta um cachorro em você"
                 ]
             }
@@ -43,14 +43,14 @@ export const perguntas = [
             {
                 texto: "Arrepia o pelo e fica em posição de ataque.",
                 afirmacao: [
-                    "Você aprendeu que nem tudo que parece ameaçador é real."
+                    "Você aprendeu que nem tudo que parece ameaçador é real.",
                     "você esta assustado e fica arisco"
                 ]
             },
             {
                 texto: "Tenta brincar com o gato do espelho.",
                 afirmacao: [
-                    "Sua natureza brincalhona transformou o susto em diversão."
+                    "Sua natureza brincalhona transformou o susto em diversão.",
                     "você fica arisco e ataca"
                 ]
             }
@@ -62,7 +62,7 @@ export const perguntas = [
             {
                 texto: "Entra em cada uma delas para testar qual é mais confortável.",
                 afirmacao: [
-                    "Você descobriu que caixas são os melhores esconderijos do mundo."
+                    "Você descobriu que caixas são os melhores esconderijos do mundo.",
                     ""
             },
             {
